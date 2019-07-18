aa2d37115afb76b27fbc685810032122f7f7212c
Kosala <kosala.amarasinghe@skipthedishes.ca>
2019-07-18 00:55:57 -0600

    FL1- generating tweets

---
+++ tweets/src/main/java/com/flyht/utils/TweetGenerator.java
@@ -0,0 +1,16 @@
+package com.flyht.utils;
+
+import java.util.List;
+
+public class TweetGenerator {
+
+    List<String> topics;
+
+    public TweetGenerator(List<String> topics) {
+        this.topics = topics;
+    }
+
+    public String generateTweet() {
+        return "";
+    }
+}
---
+++ tweets/src/test/java/com/flyht/utils/TweetGeneratorTest.java
@@ -0,0 +1,25 @@
+package com.flyht.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TweetGeneratorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void generateTweet_checkIfRuns_returnsString() {
+    }
+
+    @Test
+    public void generateTweet_checkIfTweetContainsATopic_hasOneTopicString() {
+    }
+
+    @Test
+    public void generateTweet_checkIfTweetDoesnotContainATopic_hasRandomText() {
+    }
+}
